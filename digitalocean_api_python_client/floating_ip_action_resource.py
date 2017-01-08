a27e45681fec189009e3e02ebcce512fbfac9d6c
ras <ras@ras-mbp.local>
2017-01-08 10:46:24 +0100

    reformatted code to use new instead of deprecated methods

--- digitalocean_api_python_client/floating_ip_action_resource.py
+++ digitalocean_api_python_client/floating_ip_action_resource.py
@@ -5,74 +5,42 @@ class FloatingIpActionResource(Api):
     api_uri_path = '/v2/floating_ips'
 
     def assign(self, floating_ip_address, droplet_id):
-        api_uri_query = "/{}/actions".format(floating_ip_address)
-        api_uri = "{base}{path}{query}".format(base=self.api_uri_base, path=self.api_uri_path, query=api_uri_query)
+        query = "/{}/actions".format(floating_ip_address)
 
-        request_method = "POST"
-        request_body = {"type": "assign",
-                        "droplet_id": droplet_id}
-        response_header_status_ok = 201
-        response_body_json_key = "action"
-
-        o = self.get_api_response_object(request_method,
-                                         api_uri,
-                                         response_header_status_ok,
-                                         self.generate_http_request_headers(),
-                                         request_body,
-                                         response_body_json_key)
-
-        return o
+        return self.get_object(method='POST',
+                               url=self.add_query_to_url(query),
+                               headers=self.headers,
+                               body={"type": "assign",
+                                     "droplet_id": droplet_id},
+                               response_ok=201,
+                               response_body_json_key='action')
 
     def unassign(self, floating_ip_address):
-        api_uri_query = "/{}/actions".format(floating_ip_address)
-        api_uri = "{base}{path}{query}".format(base=self.api_uri_base, path=self.api_uri_path, query=api_uri_query)
+        query = "/{}/actions".format(floating_ip_address)
 
-        request_method = "POST"
-        request_body = {"type": "unassign"}
-        response_header_status_ok = 201
-        response_body_json_key = "action"
-
-        o = self.get_api_response_object(request_method,
-                                         api_uri,
-                                         response_header_status_ok,
-                                         self.generate_http_request_headers(),
-                                         request_body,
-                                         response_body_json_key)
-
-        return o
+        return self.get_object(method='POST',
+                               url=self.add_query_to_url(query),
+                               headers=self.headers,
+                               body={"type": "unassign"},
+                               response_ok=201,
+                               response_body_json_key='action')
 
     def all(self, floating_ip_address):
-        api_uri_query = "/{}/actions".format(floating_ip_address)
-        api_uri = "{base}{path}{query}".format(base=self.api_uri_base, path=self.api_uri_path, query=api_uri_query)
+        query = "/{}/actions".format(floating_ip_address)
 
-        request_method = "GET"
-        request_body = ""
-        response_header_status_ok = 200
-        response_body_json_key = "actions"
-
-        o = self.get_api_response_objects(request_method,
-                                          api_uri,
-                                          response_header_status_ok,
-                                          self.generate_http_request_headers(),
-                                          request_body,
-                                          response_body_json_key)
-
-        return o
+        return self.get_collection(method='GET',
+                                   url=self.add_query_to_url(query),
+                                   headers=self.headers,
+                                   body=None,
+                                   response_ok=200,
+                                   response_body_json_key='actions')
 
     def find(self, floating_ip_address, action_id):
-        api_uri_query = "/{}/actions/{}".format(floating_ip_address, action_id)
-        api_uri = "{base}{path}{query}".format(base=self.api_uri_base, path=self.api_uri_path, query=api_uri_query)
+        query = "/{}/actions/{}".format(floating_ip_address, action_id)
 
-        request_method = "GET"
-        request_body = ""
-        response_header_status_ok = 200
-        response_body_json_key = "action"
-
-        o = self.get_api_response_object(request_method,
-                                         api_uri,
-                                         response_header_status_ok,
-                                         self.generate_http_request_headers(),
-                                         request_body,
-                                         response_body_json_key)
-
-        return o
+        return self.get_object(method='GET',
+                               url=self.add_query_to_url(query),
+                               headers=self.headers,
+                               body=None,
+                               response_ok=200,
+                               response_body_json_key='action')
