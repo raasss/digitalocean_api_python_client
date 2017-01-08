ad228d5966361aa6a38ed767e5d4f1fd3ec53083
ras <ras@ras-mbp.local>
2017-01-08 20:14:20 +0100

    deleted comment as it is moved to ImageResource class as method actions()

--- digitalocean_api_python_client/image_action_resource.py
+++ digitalocean_api_python_client/image_action_resource.py
@@ -4,24 +4,6 @@ from .api import Api
 class ImageActionResource(Api):
     api_uri_path = '/v2/images'
 
-    # def all(self, image_id):
-    #     api_uri_query = "/{}/actions".format(image_id)
-    #     api_uri = "{base}{path}{query}".format(base=self.api_uri_base, path=self.api_uri_path, query=api_uri_query)
-    #
-    #     request_method = "GET"
-    #     request_body = None
-    #     response_header_status_ok = 200
-    #     response_body_json_key = "actions"
-    #
-    #     o = self.get_api_response_objects(request_method,
-    #                                       api_uri,
-    #                                       response_header_status_ok,
-    #                                       self.generate_http_request_headers(),
-    #                                       request_body,
-    #                                       response_body_json_key)
-    #
-    #     return o
-
     def transfer(self, image_id, region):
         query = "/{}/actions".format(image_id)
 
