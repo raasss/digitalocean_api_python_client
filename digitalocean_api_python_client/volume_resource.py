64aa56549cc4d9d7bc87707c2540100ef3cc3c54
ras <ras@ras-mbp.local>
2017-01-09 09:55:55 +0100

    - changed deprecated with new methods - reordered methods like they are in documentation - added snapshots(), create_snapshot() and delete_by_name methods

--- digitalocean_api_python_client/volume_resource.py
+++ digitalocean_api_python_client/volume_resource.py
@@ -35,15 +35,6 @@ class VolumeResource(Api):
                                response_ok=201,
                                response_body_json_key='volume')
 
-    def delete(self, volume_id):
-        query = "/{}".format(volume_id)
-
-        return self.request(method='DELETE',
-                            url=self.add_query_to_url(query),
-                            headers=self.headers,
-                            body=None,
-                            response_ok=204)
-
     def find(self, id):
         logging.info('Retrieve an existing Block Storage volume. (id={})'.format(id))
 
@@ -56,11 +47,8 @@ class VolumeResource(Api):
                                response_ok=200,
                                response_body_json_key='volume')
 
-    def find_by_name(self, name, region=None, page=None, per_page=None):
-        query = '?page={}&per_page={}&name={}'.format(page or 1, per_page or self.per_page, name)
-
-        if region is not None:
-            query += '&region={}'.format(region)
+    def find_by_name(self, name, region, page=None, per_page=None):
+        query = '?name={}&region={}&page={}&per_page={}'.format(name, region, page or 1, per_page or self.per_page)
 
         return self.get_paginator(method='GET',
                                   url=self.add_query_to_url(query),
@@ -70,3 +58,44 @@ class VolumeResource(Api):
                                   response_body_json_key='volumes',
                                   page=page,
                                   per_page=per_page)
+
+    def snapshots(self, volume_id, page=None, per_page=None):
+        query = '/{}/snapshots?page={}&per_page={}'.format(volume_id, page or 1, per_page or self.per_page)
+
+        return self.get_paginator(method='GET',
+                                  url=self.add_query_to_url(query),
+                                  headers=self.headers,
+                                  body=None,
+                                  response_ok=200,
+                                  response_body_json_key='snapshots',
+                                  page=page,
+                                  per_page=per_page)
+
+    def create_snapshot(self, volume_id, snapshot_name):
+        query = "/{}".format(volume_id)
+
+        return self.get_object(method='POST',
+                               url=self.add_query_to_url(query),
+                               headers=self.headers,
+                               body={"name": snapshot_name},
+                               response_ok=201,
+                               response_body_json_key='snapshot')
+
+    def delete(self, volume_id):
+        query = "/{}".format(volume_id)
+
+        return self.request(method='DELETE',
+                            url=self.add_query_to_url(query),
+                            headers=self.headers,
+                            body=None,
+                            response_ok=204)
+
+    def delete_by_name(self, volume_name, region_slug):
+        query = "?name={}&region={}".format(volume_name, region_slug)
+
+        return self.request(method='DELETE',
+                            url=self.add_query_to_url(query),
+                            headers=self.headers,
+                            body=None,
+                            response_ok=204)
+
