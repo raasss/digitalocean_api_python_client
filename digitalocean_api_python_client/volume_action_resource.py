f0777545053921dc2d5b3fe210036dda6b8c7d3d
ras <ras@ras-mbp.local>
2017-01-09 09:31:25 +0100

    - changed deprecated with new methods - added attach_by_name() and detach_by_name() methods

--- digitalocean_api_python_client/volume_action_resource.py
+++ digitalocean_api_python_client/volume_action_resource.py
@@ -1,6 +1,5 @@
 from .api import Api
 import logging
-import time
 
 
 class VolumeActionResource(Api):
@@ -21,6 +20,20 @@ class VolumeActionResource(Api):
                                response_ok=202,
                                response_body_json_key='action')
 
+    def attach_by_name(self, volume_name, droplet_id, region):
+        query = "/actions"
+
+        request_body = {'type': 'attach',
+                        'volume_name': volume_name,
+                        'region': region,
+                        'droplet_id': droplet_id}
+
+        return self.get_object(method='POST',
+                               url=self.add_query_to_url(query),
+                               headers=self.headers,
+                               body=request_body,
+                               response_ok=202,
+                               response_body_json_key='action')
 
     def detach(self, volume_id, droplet_id, region):
         query = "/{}/actions".format(volume_id)
@@ -36,28 +49,38 @@ class VolumeActionResource(Api):
                                response_ok=202,
                                response_body_json_key='action')
 
-    def resize(self, volume_id, size_gigabytes, region):
-        api_uri_query = "/{}/actions".format(volume_id)
-        api_uri = "{base}{path}{query}".format(base=self.api_uri_base, path=self.api_uri_path, query=api_uri_query)
+    def detach_by_name(self, volume_name, droplet_id, region):
+        query = "/actions"
 
-        request_method = "POST"
+        request_body = {'type': 'detach',
+                        'droplet_id': droplet_id,
+                        'volume_name': volume_name,
+                        'region': region}
+
+        return self.get_object(method='POST',
+                               url=self.add_query_to_url(query),
+                               headers=self.headers,
+                               body=request_body,
+                               response_ok=202,
+                               response_body_json_key='action')
+
+    def resize(self, volume_id, size_gigabytes, region):
+        query = "/{}/actions".format(volume_id)
+
         request_body = {'type': 'resize',
                         'size_gigabytes': size_gigabytes,
                         'region': region}
-        response_header_status_ok = 202
-        response_body_json_key = "action"
 
-        o = self.get_api_response_object(request_method,
-                                         api_uri,
-                                         response_header_status_ok,
-                                         self.generate_http_request_headers(),
-                                         request_body,
-                                         response_body_json_key)
-
-        return o
+        return self.get_object(method='POST',
+                               url=self.add_query_to_url(query),
+                               headers=self.headers,
+                               body=request_body,
+                               response_ok=202,
+                               response_body_json_key='action')
 
     def all(self, volume_id, page=None, per_page=None):
-        logging.info('List all actions for a volume. (volume_id={}, page={}, per_page={})'.format(volume_id, page, per_page))
+        logging.info(
+            'List all actions for a volume. (volume_id={}, page={}, per_page={})'.format(volume_id, page, per_page))
 
         query = '/{}/actions?page={}&per_page={}'.format(volume_id, page or 1, per_page or self.per_page)
 
@@ -76,8 +99,8 @@ class VolumeActionResource(Api):
         query = "/{}/actions/{}".format(volume_id, action_id)
 
         return self.get_object(method='GET',
-                            url=self.add_query_to_url(query),
-                            headers=self.headers,
-                            body=None,
-                            response_ok=200,
-                            response_body_json_key='action')
+                               url=self.add_query_to_url(query),
+                               headers=self.headers,
+                               body=None,
+                               response_ok=200,
+                               response_body_json_key='action')
