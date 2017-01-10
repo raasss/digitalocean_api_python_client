2321b83deddf792a119d318701acd00dcd1d6741
ras <ras@ras-mbp.local>
2017-01-10 14:36:48 +0100

    added testing code for missing clien.ssh_keys methods

--- examples/deploy-lab-env.py
+++ examples/deploy-lab-env.py
@@ -8,22 +8,78 @@ import time
 client = digitalocean_api_python_client.Client()
 
 ###############################################################################
-###############################################################################
+### testing client.ssh_keys.all()
 ###############################################################################
 
 print
 print('### Getting all SSH Keys ###')
 print
 
+print('>>> Fetching all SSH Keys associated with DigitalOcean account...\n')
 ssh_keys = client.ssh_keys.all()
 
 my_ssh_keys = []
 for ssh_key in ssh_keys:
-    print('>>> Found ssh_key: {}'.format(vars(ssh_key)))
+    print('{}\n'.format(ssh_key))
     my_ssh_keys.append(ssh_key.fingerprint)
 
-print
-print('my_ssh_keys = {}'.format(my_ssh_keys))
+print('>>> While printing fetched ssh keys we already generated array of key fingerprints to be used in droplet '
+      'creation.\n')
+print('my_ssh_keys = {}\n'.format(my_ssh_keys))
+
+temp_ssh_key_fingerprint = 'f0:13:aa:90:5c:2f:7f:6d:dd:5b:55:0b:40:07:18:de'
+temp_ssh_key_def = {'name': 'Temp SSH Public Key',
+                    'public_key': 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCjQFINvxMD/3ZZZUGOZrtQ2ltNWHC1GYWGToVmUYKcnTZL62T+fg1I0zFA0aI66ZmE2Wxv5k/M02gdunAeqsknYH8VLSTqcqNoYjFxJ7uvfEIbFq/pGjH/dugoOlS4duUmc0OAgbHykLU5X5Qi/FM5wHyZpuvGavCDxHh8aLtAMSL2t28cVVKggyEppW94d+uuudphKtT5PDLV6Q0WkDa312QoghkklbUD6kKh8oAkFgpVXTvX3yQ3fpAI/hCHhJ2tihuMpgjjoIdHgqSAo9IfTc7YSXrTUx81/zeBlASUrZMRiY8UFwDbBmAGG4H3nDRqNELlZNPYEiCeHUCXtMhF temp_public_key'}
+
+print('>>> Checking if Temp SSH key with known fingerprint already exists\n')
+try:
+    temp_ssh_key = client.ssh_keys.find(temp_ssh_key_fingerprint)
+except ValueError:
+    temp_ssh_key = None
+
+if temp_ssh_key is not None:
+    print('>>> Found Temp SSH key\n')
+    print(temp_ssh_key)
+
+    print('>>> Deleting Temp SSH key with fingerprint {}\n'.format(temp_ssh_key_fingerprint))
+    client.ssh_keys.delete(temp_ssh_key_fingerprint)
+else:
+    print('>>> Temp SSH key with fingerprint {} does not exist.\n'.format(temp_ssh_key_fingerprint))
+
+###############################################################################
+### testing client.ssh_keys.create()
+###############################################################################
+
+print('>>> Adding Temp SSH key to DigitalOcean account...\n')
+temp_ssh_key = client.ssh_keys.create(temp_ssh_key_def)
+print(temp_ssh_key)
+
+###############################################################################
+### testing client.ssh_keys.update()
+###############################################################################
+
+print('>>> Updating Temp SSH key to "Temp SSH Public Key - updated\n')
+temp_ssh_key_updated = client.ssh_keys.update(temp_ssh_key.id, 'Temp SSH Public Key - updated')
+if temp_ssh_key_updated.name == 'Temp SSH Public Key - updated':
+    print(temp_ssh_key_updated)
+else:
+    raise RuntimeError('Temp SSH Public Key name updating failed!')
+
+###############################################################################
+### testing client.ssh_keys.find()
+###############################################################################
+
+print('>>> Finding Temp SSH Public Key by already known fingerprint\n')
+temp_ssh_key_updated2 = client.ssh_keys.find(temp_ssh_key_fingerprint)
+if temp_ssh_key_updated2.fingerprint == temp_ssh_key_fingerprint:
+    print('>>> Found Temp SSH Public Key by fingerprint {}\n'.format(temp_ssh_key_fingerprint))
+    print(temp_ssh_key_updated2)
+
+print('>>> Finding Temp SSH Public Key by already known ID\n')
+temp_ssh_key_updated2 = client.ssh_keys.find(temp_ssh_key_updated.id)
+if temp_ssh_key_updated2.fingerprint == temp_ssh_key_fingerprint:
+    print('>>> Found Temp SSH Public Key by ID {}\n'.format(temp_ssh_key_updated.id))
+    print(temp_ssh_key_updated2)
 
 ###############################################################################
 ###############################################################################
@@ -49,7 +105,7 @@ droplets = client.droplets.all()
 i = 0
 for droplet in droplets:
     print('>>> Found droplet: {}'.format(vars(droplet)))
-    if droplet.name == droplet1_def['name']:
+    if droplet.name == droplet1_def['name'] && droplet.region == droplet1_def['region']:
         print('>>> Droplet already created!')
         droplet1 = droplet
         i += 1
