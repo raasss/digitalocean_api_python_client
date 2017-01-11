5d9630b2715d4bf4dbfebe82898d45aef1e84089
ras <ras@ras-mbp.local>
2017-01-11 13:41:51 +0100

    Revert "Merge remote-tracking branch 'origin/master' into develop"
    This reverts commit efb9a0b7ff4f992934da1d8f89ab542a02ee2e92, reversing
    changes made to 2321b83deddf792a119d318701acd00dcd1d6741.

--- digitalocean_api_python_client/ssh_key.py
+++ digitalocean_api_python_client/ssh_key.py
@@ -10,9 +10,4 @@ class SshKey(object):
         self.name = name
 
     def __repr__(self):
-        s = 'id          : {}\n'.format(self.id)
-        s += 'fingerprint : {}\n'.format(self.fingerprint)
-        s += 'public_key  : {}\n'.format(self.public_key)
-        s += 'name        : {}'.format(self.name)
-
-        return s
+        return str(vars(self))
