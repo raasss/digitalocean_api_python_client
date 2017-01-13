be6287b2c7d6c9056c64c610847df7e18ae2f200
ras <ras@ras-mbp.local>
2017-01-13 23:28:20 +0100

    testing...

--- digitalocean_api_python_client/account.py
+++ digitalocean_api_python_client/account.py
@@ -1,5 +1,22 @@
 class Account(object):
-    """Account object as per https://developers.digitalocean.com/documentation/v2/#account"""
+    """Account object as per https://developers.digitalocean.com/documentation/v2/#account
+
+    :param droplet_limit: The total number of droplets the user may have.
+    :param floating_ip_limit: The total number of floating IPs the user may have.
+    :param email: The email the user has registered for Digital Ocean with.
+    :param uuid: The universal identifier for this user (alphanumeric).
+    :param email_verified: If true, the user has verified their account via email. False otherwise.
+    :param status: This value is one of "active", "warning" or "locked".
+    :param status_message: A human-readable message giving more details about the status of the account.
+
+    :type droplet_limit: number
+    :type floating_ip_limit: number
+    :type email: string
+    :type uuid: string (alphanumeric)
+    :type email_verified: boolean
+    :type status: string
+    :type status_message: string
+    """
 
     def __init__(self,
                  droplet_limit,
@@ -9,27 +26,13 @@ class Account(object):
                  email_verified,
                  status,
                  status_message):
-
         self.droplet_limit = droplet_limit
-        """(int) The total number of droplets the user may have."""
-
         self.floating_ip_limit = floating_ip_limit
-        """(int) The total number of floating IPs the user may have."""
-
         self.email = email
-        """(str) The email the user has registered for Digital Ocean with."""
-
         self.uuid = uuid
-        """(str) The universal identifier for this user (alphanumeric)."""
-
         self.email_verified = email_verified
-        """(bool) If true, the user has verified their account via email. False otherwise."""
-
         self.status = status
-        """(str) This value is one of "active", "warning" or "locked"."""
-
         self.status_message = status_message
-        """(str) A human-readable message giving more details about the status of the account."""
 
     def __repr__(self):
         s = 'uuid             : {}\n'.format(self.uuid)
