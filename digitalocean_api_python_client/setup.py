b360c8ac3b4f58974212ab4a605016dec2439f90
ras <ras@ras-mbp.local>
2017-01-13 13:55:31 +0100

    added setup.py

---
+++ digitalocean_api_python_client/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='digitalocean_api_python_client',
+      version='0.1',
+      description='DigitalOcean API Python Client',
+      url='https://github.com/raasss/digitalocean_api_python_client',
+      author='Radisa Jovicevic',
+      author_email='radisa@gmail.com',
+      license='GPL',
+      packages=['digitalocean_api_python_client'],
+      zip_safe=False)
